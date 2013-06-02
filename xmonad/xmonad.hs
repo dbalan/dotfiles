2316ab08823cd92a0267a0864b17725439556dd0
Dhananjay Balan <mb.dhananjay@gmail.com>
2013-06-02 21:53:39 +0530

    Looks good for a skelteton. Didnt test.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -31,10 +31,7 @@ import XMonad.Actions.CopyWindow
 
 baseConf = gnomeConfig
 
-ws_eclipse = "4:eclipse"
-ws_im = "9:pidgin"
-
-myWorkspaces = ["1:terminal","2:browser","3:editor",ws_eclipse,"5:eclipse-2","6:gitk","7:graphics","8:mail",ws_im,"0:misc","-","="]
+myWorkspaces = ["1:terminal","2:browser","3:editor","4:runtime","5:files","6:runtime","7:graphics","8:mail","9:misc","0:misc","-","="]
 isFullscreen = (== "fullscreen")
 
 myManageHook = composeAll
@@ -42,11 +39,11 @@ myManageHook = composeAll
                [ resource  =? "Do"   --> doIgnore
                , className =? "Emacs" --> doShift "3:editor"
                , resource  =? "gnome-terminal" --> doShift "1:terminal"
-               , className =? "Eclipse" --> doShift ws_eclipse
+               , className =? "Eclipse" --> doShift "3:editor"
                , (className =? "Firefox" <||> className =? "Opera") --> doShift "2:browser"
                , (className =? "Google-chrome" <||> className =? "Chromium-browser") --> doShift "2:browser"
                , className =? "Thunderbird" --> doShift "8:mail"
-               , className =? "Pidgin" --> doShift ws_im
+               , className =? "Pidgin" --> doShift "8:mail"
                , className =? "Gitk" --> doShift "9:gitk"
                , className =? "Xmessage"  --> doFloat
                , className =? "qemu" --> doFloat
@@ -60,8 +57,8 @@ pidginLayout = withIM ratio roster chatLayout where
 myLayoutHook =  avoidStruts $ eclipse $ pidgin $ normal
   where
     normal = Full
-    eclipse = onWorkspace ws_eclipse Full
-    pidgin = onWorkspace ws_im pidginLayout
+    eclipse = onWorkspace "3:editor" Full
+    pidgin = onWorkspace "8:mail" pidginLayout
 
 -- Run xmobar
 
@@ -77,25 +74,29 @@ alt key = "M1-" ++ key
 win key = "M-" ++ key
 
 myKeys =
-  [ (win "<Space>", spawn "gnome-do")
-  , (win "l", spawn "xflock4")
+  [ (win "<Space>", spawn "synapse")
   , (win "`", sendMessage NextLayout)
-  , (win "w", spawn "google-chrome")
-    -- , ("M-S-`", setLayout $ XMonad.layoutHook baseConf)
 
-    -- use classic "WIN"+TAB
+  , (win "w", spawn "google-chrome")
+  , (win "t", spawn "terminator")
+  , (win "f", spawn "thunar")
+
+  , ("M-S-`", setLayout $ XMonad.layoutHook baseConf)
+
+  -- use classic "WIN"+TAB
   , (win "<Tab>", windows W.focusDown)
   , (win "S-<Tab>", windows W.focusUp)
 
-    -- mac-ish windows
+  -- mac-ish windows
   , (win "q", kill) -- kil the current app
   , ("C-S-q", spawn "gnome-session-quit")
-    -- moving workspaces
+  -- moving workspaces
   , ("C-S-<Left>", prevWS)
   , ("C-S-<Right>", nextWS)
   , (win "S-<Left>", shiftToPrev)
   , (win "S-<Right>", shiftToNext)
   ]
+
 main :: IO ()
 main = do
   d <- spawnPipe $ "xmobar /home/dhananjay/.xmobarrc"
